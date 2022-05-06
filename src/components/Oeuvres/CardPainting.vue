a92c0c13a68e19f085fad451afbc306f1f9c3d12
Adam Djafri <adam.djafri@gmail.com>
2022-05-06 16:34:21 +0200

    WIP : card accueil

---
+++ src/components/Oeuvres/CardPainting.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="q-pa-md row items-start q-gutter-md">
+    <q-card class="my-card">
+      <img src="https://placeimg.com/500/200/nature" >
+
+      <q-card-section>
+        <div class="text-h6">Nom oeuvre</div>
+        <div class="text-subtitle2">Lorem ipsum dolor, sit amet consectetur adipisicing elit.</div>
+      </q-card-section>
+
+      <q-card-section class="q-pt-none">
+        {{ lorem }}
+      </q-card-section>
+    </q-card>
+    <q-card class="my-card">
+      <img src="https://placeimg.com/500/200/nature" >
+
+      <q-card-section>
+        <div class="text-h6">Nom oeuvre <q-btn to="/start/pick-quasar-flavour" label="Sculpture" outline color="purple"/> <q-btn to="/start/pick-quasar-flavour" label="Artiste" outline color="green"/></div>
+        
+        <div class="text-subtitle2">Lorem ipsum dolor, sit amet consectetur adipisicing elit.</div>
+      </q-card-section>
+
+      <q-card-section class="q-pt-none">
+        {{ lorem }}
+      </q-card-section>
+    </q-card>
+  </div>
+</template>
--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,17 +1,14 @@
 <template>
   <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-vertical.svg"
-      style="width: 200px; height: 200px"
-    >
+      <CardPainting/>
   </q-page>
 </template>
 
 <script>
-import { defineComponent } from 'vue'
+import CardPainting from 'components/Oeuvres/CardPainting.vue'
 
-export default defineComponent({
-  name: 'IndexPage'
+export default ({
+  name: 'IndexPage',
+  components: { CardPainting }
 })
 </script>
