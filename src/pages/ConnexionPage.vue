ba2a4cf4074d280e98d0741a916ae4c0549173bb
Alexis Vedrenne <alexisvedrenne482@gmail.com>
2022-05-06 15:17:22 +0200

    Design de la page de connexion

---
+++ src/pages/ConnexionPage.vue
@@ -0,0 +1,39 @@
+<template>
+  <q-layout class="bg-accent row items-center" view="lHh Lpr lFf">
+    <div class="row col items-center">
+      <section class="col">
+        <div class="row justify-center">
+          <q-img style="height: 70%; width: 70%" src="~assets/logoMuseo.png" />
+        </div>
+      </section>
+      <main class="col row items-center q-pt-xl q-mr-xl">
+        <section class="col">
+          <q-input class="q-mb-lg" color="secondary" label="Entrer votre identifiant">
+            <template v-slot:prepend> <q-icon color="grey-10" name="person" /> </template
+          ></q-input>
+          <q-input
+            class="q-mb-xl"
+            color="secondary"
+            type="password"
+            label="Entrer votre mot de passe"
+          >
+            <template v-slot:prepend> <q-icon color="grey-10" name="lock" /> </template
+          ></q-input>
+          <div class="row justify-center">
+            <q-btn
+              class="q-pt-sm q-pb-sm"
+              style="width: 180px"
+              color="secondary"
+              no-caps
+              label="Connexion"
+            />
+          </div>
+        </section>
+      </main>
+    </div>
+  </q-layout>
+</template>
+
+<script>
+export default {};
+</script>
