67a7a85e1c73aa2243ed08d4cecc7f786bedea1e
Alexis Vedrenne <alexisvedrenne482@gmail.com>
2022-05-23 09:24:43 +0200

    Ajout du code de sana

--- src/components/artiste/CardArtiste.vue
+++ src/components/artiste/CardArtiste.vue
@@ -14,10 +14,69 @@
         <q-expansion-item expand-separator label="Bibliographie"> </q-expansion-item>
       </div>
       <div class="col row justify-end">
-        <q-btn flat color="secondary" icon="edit" />
+        <q-btn @click="edit = true" flat color="secondary" icon="edit" />
         <q-btn flat color="negative" icon="delete" />
         <q-btn text-color="primary" color="accent" no-caps label="Voir les oeuvres" />
       </div>
     </div>
   </q-card>
 </template>
+<script>
+import { useQuasar } from "quasar";
+export default {
+  props: {
+    id: {
+      type: String,
+    },
+    proArtiste: {
+      type: Object,
+      required: true,
+    },
+  },
+  data() {
+    return {
+      utils: useQuasar(),
+      loading: false,
+      edit: false,
+      artiste: this.proArtiste,
+    };
+  },
+  mounted() {},
+  methods: {
+    async submit() {
+      this.loading = true;
+      await this.$store.dispatch("updateArtiste", {
+        artiste: this.artiste,
+        id: this.id,
+      });
+      this.loading = false;
+      this.edit = false;
+    },
+    deleteArtiste() {
+      this.utils
+        .dialog({
+          title: "Attention !",
+          message:
+            "Vous etes sur de vouloir retirer le musée '" + this.artiste.nom + "' ?",
+          cancel: true,
+          persistent: true,
+          ok: {
+            label: "Supprimer",
+            color: "negative",
+            flat: true,
+          },
+          cancel: {
+            label: "Annuler",
+            flat: true,
+            color: "primary",
+          },
+        })
+        .onOk(async () => {
+          this.utils.loading.show();
+          await this.$store.dispatch("deleteArtiste", { id: this.id });
+          this.utils.loading.hide();
+        });
+    },
+  },
+};
+</script>
