9f3ab26e9630eb5937e761a72b64ccc46ac896a0
Alexis Vedrenne <alexisvedrenne482@gmail.com>
2022-05-23 09:17:35 +0200

    Ajout du code de sana

--- src/pages/artiste/ListeArtistePage.vue
+++ src/pages/artiste/ListeArtistePage.vue
@@ -1,28 +1,109 @@
 <template>
   <q-page>
-    <q-card square class="bg-primary"
-      ><q-card-section
-        ><p class="text-white text-center text-bold" style="font-size: 30px">
-          Liste des Artistes
-        </p></q-card-section
-      ></q-card
-    >
-    <main v-if="artistes">
-      <q-intersection
-        transition="scale"
-        v-for="(artiste, index) in artistes"
-        :key="index"
-        class="q-ma-md"
+    <main>
+      <q-card square class="bg-primary"
+        ><q-card-section
+          ><p class="text-white text-center text-bold" style="font-size: 30px">
+            Liste des artistes
+          </p></q-card-section
+        ></q-card
       >
-        <CardArtiste />
-      </q-intersection>
-      <div v-if="artistes.length == 0">
-        <p class="text-grey text-center q-mt-lg">Aucun artiste(s) trouvés...</p>
+      <div v-if="artistes">
+        <q-intersection
+          transition="scale"
+          v-for="(artiste, index) in artistes"
+          :key="index"
+          class="q-ma-md"
+        >
+          <CardArtiste :id="res.docs[index].id" :proArtiste="artiste" />
+        </q-intersection>
+
+        <div v-if="artiste.length == 0">
+          <p class="text-grey text-center q-mt-lg">Aucun artiste trouvés...</p>
+        </div>
       </div>
+      <div v-else class="q-mt-xl row justify-center">
+        <q-spinner-puff color="primary" size="50px" />
+      </div>
+      <q-page-sticky position="bottom-right" :offset="[18, 18]">
+        <q-btn @click="add = true" fab icon="add" color="secondary" />
+      </q-page-sticky>
     </main>
+    <q-dialog
+      transition-show="flip-down"
+      transition-hide="flip-up"
+      persistent
+      v-model="add"
+    >
+      <q-card style="width: 500px">
+        <q-card-section class="row items-center q-pb-none">
+          <div class="text-h6">Ajout d'un artiste</div>
+          <q-space />
+          <q-btn icon="close" flat round dense v-close-popup />
+        </q-card-section>
+
+        <q-card-section>
+          <q-form @submit="submit">
+            <div class="row">
+              <q-input
+                lazy-rules
+                :rules="[
+                  (val) =>
+                    (val && val.length > 0) || 'Le nom du artiste doit être saisie !',
+                ]"
+                v-model="artiste.nom"
+                color="secondary"
+                class="col q-mr-md"
+                label="Nom"
+              ></q-input>
+            </div>
+            <div class="row">
+              <q-input
+                lazy-rules
+                :rules="[
+                  (val) =>
+                    (val && val.length > 0) || 'Le prenom du artiste doit être saisie !',
+                ]"
+                v-model="artiste.prenom"
+                color="secondary"
+                class="col q-mr-md"
+                label="Prenom"
+              ></q-input>
+            </div>
+            <div class="row">
+              <q-input
+                v-model="artiste.image"
+                color="secondary"
+                class="col q-mr-md"
+                label="image"
+              ></q-input>
+            </div>
+
+            <div class="row">
+              <q-input
+                v-model="artiste.bibliographie"
+                color="secondary"
+                class="col q-mr-md"
+                label="bibliographie"
+              ></q-input>
+            </div>
+
+            <div class="row justify-center q-mt-lg">
+              <q-btn
+                :loading="loading"
+                type="submit"
+                style="border-raduis: 10px"
+                label="Ajouter"
+                no-caps
+                color="secondary"
+              />
+            </div>
+          </q-form>
+        </q-card-section>
+      </q-card>
+    </q-dialog>
   </q-page>
 </template>
-
 <script>
 import CardArtiste from "components/artiste/CardArtiste.vue";
 import { collection, getDocs, query, onSnapshot } from "firebase/firestore";
@@ -33,7 +114,15 @@ export default {
   },
   data() {
     return {
-      artistes: [],
+      add: false,
+      loading: false,
+      artistes: null,
+      res: null,
+      artiste: {
+        nom: "",
+        adresse: "",
+        tel: "",
+      },
     };
   },
   async mounted() {
@@ -55,8 +144,24 @@ export default {
   methods: {
     async refresh() {
       this.artistes = null;
-      let artistes = await this.$store.dispatch("fetchAllArtist");
-      this.artistes = artistes.docs;
+      this.res = await this.$store.dispatch("fetchAllArtist");
+      let artistes = [];
+      for (let i = 0; i < this.res.docs.length; i++) {
+        artistes.push(this.res.docs[i].data());
+      }
+
+      this.artistes = Object.values(artistes);
+    },
+    async submit() {
+      this.loading = true;
+      await this.$store.dispatch("addArtiste", { artiste: this.artiste });
+      this.loading = false;
+      this.add = false;
+      this.artiste = {
+        nom: "",
+        adresse: "",
+        tel: "",
+      };
     },
   },
 };
