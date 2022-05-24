c3aa834deddf5f178ea35a83054f9fc1eda74483
Alexis Vedrenne <alexisvedrenne482@gmail.com>
2022-05-24 10:16:23 +0200

    Design filtre et recherche pour oeuvre

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,5 +1,86 @@
 <template>
   <q-page>
+    <q-card class="bg-primary text-white" square
+      ><q-card-section class="row items-center"
+        ><q-select
+          class="col-2 q-mr-md"
+          color="accent"
+          bg-color="secondary"
+          filled
+          v-model="sss"
+          :options="artistes"
+          label="Choisir un artiste"
+        >
+          <template v-slot:option="artistes">
+            <q-item v-bind="artistes.itemProps">
+              <q-item-section>
+                <q-avatar> <q-img :src="artistes.opt.image" /></q-avatar>
+              </q-item-section>
+              <q-item-section>
+                <q-item-label
+                  >{{ artistes.opt.nom + " " + artistes.opt.prenom }}
+                </q-item-label>
+              </q-item-section>
+            </q-item>
+          </template></q-select
+        ><q-select
+          class="col-2 q-mr-md"
+          color="accent"
+          bg-color="secondary"
+          filled
+          v-model="model"
+          :options="types"
+          label="Choisir un type d'oeuvre"
+        >
+          <template v-slot:option="types">
+            <q-item v-bind="types.itemProps">
+              <q-item-section avatar>
+                <div
+                  :style="
+                    'background-color:' +
+                    types.opt.couleur +
+                    ';border-radius: 30px;width: 40px;height: 40px'
+                  "
+                ></div>
+              </q-item-section>
+              <q-item-section>
+                <q-item-label>{{ types.opt.nom }}</q-item-label>
+              </q-item-section>
+            </q-item>
+          </template></q-select
+        ><q-select
+          class="col-2 q-mr-md"
+          color="accent"
+          bg-color="secondary"
+          filled
+          v-model="model"
+          :options="status"
+          label="Status"
+          clearable
+        >
+          <template v-slot:option="status">
+            <q-item v-bind="status.itemProps">
+              <q-item-section avatar>
+                <q-icon :name="status.opt.icon" />
+              </q-item-section>
+              <q-item-section>
+                <q-item-label>{{ status.opt.label }}</q-item-label>
+              </q-item-section>
+            </q-item>
+          </template>
+        </q-select>
+        <q-input
+          color="secondary"
+          bg-color="white"
+          outlined
+          v-model="text"
+          class="col q-mr-sm"
+          label="Rechercher une oeuvre..."
+        >
+          <template v-slot:prepend> <q-icon name="search" /> </template
+        ></q-input>
+        <q-btn color="secondary" flat icon="search" /> </q-card-section
+    ></q-card>
     <div v-if="this.$route.params.idMusee" class="row">
       <q-btn
         :to="{ name: 'listMusee' }"
@@ -75,6 +156,30 @@ export default {
     return {
       utils: useQuasar(),
       oeuvres: null,
+      types: [],
+      artistes: [],
+      status: [
+        {
+          label: "exposition",
+          value: "exposition",
+          icon: "filter_frames",
+        },
+        {
+          label: "stock",
+          icon: "inventory_2",
+          value: "stock",
+        },
+        {
+          label: "prét",
+          icon: "real_estate_agent",
+          value: "prét",
+        },
+        {
+          label: "restauration",
+          icon: "brush",
+          value: "restauration",
+        },
+      ],
     };
   },
   async mounted() {
@@ -89,23 +194,43 @@ export default {
     }
   },
   methods: {
+    async refreshOptions() {
+      let types = await this.$store.dispatch("fetchAllTypeOeuvre");
+      let artistes = await this.$store.dispatch("fetchAllArtist");
+      types.docs.forEach((type) => {
+        let data = type.data();
+        if (!this.types.find((element) => element.nom === data.nom)) {
+          this.types.push(data);
+        }
+      });
+      artistes.forEach((artiste) => {
+        let data = artiste.data();
+        if (!this.artistes.find((element) => element.nom === data.nom)) {
+          this.artistes.push(data);
+        }
+      });
+    },
     async refresh() {
+      await this.refreshOptions();
       this.oeuvres = null;
       this.oeuvres = await this.$store.dispatch("fetchAllOeuvres");
     },
     async refreshByArtiste() {
+      await this.refreshOptions();
       this.oeuvres = null;
       this.oeuvres = await this.$store.dispatch("fetchOeuvreByArtiste", {
         idArtiste: this.$route.params.idArtiste,
       });
     },
     async refreshByMusee() {
+      await this.refreshOptions();
       this.oeuvres = null;
       this.oeuvres = await this.$store.dispatch("fetchOeuvreByMusee", {
         idMusee: this.$route.params.idMusee,
       });
     },
     async refreshByType() {
+      await this.refreshOptions();
       this.oeuvres = null;
       this.oeuvres = await this.$store.dispatch("fetchOeuvreByType", {
         idType: this.$route.params.idType,
