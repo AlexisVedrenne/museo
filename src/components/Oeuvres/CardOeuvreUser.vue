45ec9054aed212ea3beb5995cb6256b16df0f73a
HugoDucci <duccihugo@gmail.com>
2022-06-13 10:26:29 +0200

    [WIP] Add popup user

--- src/components/Oeuvres/CardOeuvreUser.vue
+++ src/components/Oeuvres/CardOeuvreUser.vue
@@ -39,7 +39,40 @@
             <p class="text-justify">{{ oeuvre.briefDescrition }}</p>
           </q-expansion-item>
           <div class="col-1 q-mr-md q-ml-md">
-            <q-btn push size="18px" round icon="search" color="secondary" />
+            <q-btn
+              push
+              size="18px"
+              round
+              icon="search"
+              color="secondary"
+              @click="dialog = true"
+            />
+            <q-dialog v-model="dialog">
+              <q-card>
+                <q-card-section class="row items-center">
+                  <q-avatar icon="signal_wifi_off" color="primary" text-color="white" />
+                  <span class="q-ml-sm"
+                    >You are currently not connected to any network.</span
+                  >
+                </q-card-section>
+
+                <q-card-section class="row items-center">
+                  <q-toggle v-model="cancelEnabled" label="Cancel button enabled" />
+                </q-card-section>
+
+                <!-- Notice v-close-popup -->
+                <q-card-actions align="right">
+                  <q-btn
+                    flat
+                    label="Cancel"
+                    color="primary"
+                    v-close-popup="cancelEnabled"
+                    :disable="!cancelEnabled"
+                  />
+                  <q-btn flat label="Turn on Wifi" color="primary" v-close-popup />
+                </q-card-actions>
+              </q-card>
+            </q-dialog>
           </div>
         </div>
       </div>
@@ -92,6 +125,7 @@ export default {
       utils: useQuasar(),
       artiste: {},
       oeuvre: this.proOeuvre,
+      dialog: false,
     };
   },
   async mounted() {
