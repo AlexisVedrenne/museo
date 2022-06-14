5fa270dfc264eb846cec87bff634342ed3b984b8
Adam Djafri <adam.djafri@gmail.com>
2022-06-14 11:57:59 +0200

    Map localisation

--- src/components/Oeuvres/CardPlan.vue
+++ src/components/Oeuvres/CardPlan.vue
@@ -4,7 +4,7 @@
       square
       size="130px"
       font-size="50px"
-      color="secondary"
+      color="accent"
       text-color="blue-grey-10"
       style="
         margin-left: 300px;
@@ -18,20 +18,25 @@
       square
       size="130px"
       font-size="50px"
-      color="secondary"
+      color="accent"
       text-color="blue-grey-10"
-      style="border-radius: 10px; margin-top: -2px; border: 1px solid black"
+      style="
+        border-radius: 10px;
+        margin-top: -153px;
+        margin-left: 430px;
+        border: 1px solid black;
+      "
       >1<q-icon color="negative" name="pin_drop"
     /></q-avatar>
     <q-avatar
       square
       font-size="50px"
-      color="secondary"
+      color="accent"
       text-color="blue-grey-10"
       style="
         margin-left: 300px;
         border-bottom-left-radius: 10px;
-        margin-top: -3px;
+        margin-top: -21px;
         border: 1px solid black;
         width: 130px;
         height: 200px;
@@ -42,10 +47,11 @@
       square
       font-size="50px"
       size="150px"
-      color="secondary"
+      color="accent"
       text-color="blue-grey-10"
       style="
-        margin-top: 45px;
+        margin-top: -170px;
+        margin-left: 430px;
         border: 1px solid black;
         border-bottom-right-radius: 40px;
         border-top-right-radius: 40px;
@@ -56,10 +62,10 @@
       square
       font-size="50px"
       size="150px"
-      color="secondary"
+      color="accent"
       text-color="blue-grey-10"
       style="
-        margin-top: 45px;
+        margin-top: -170px;
         margin-left: -430px;
         border: 1px solid black;
         border-bottom-left-radius: 40px;
